276fa3fb3b0ddb366353979dde63e63fc1296382
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2023-12-17 15:16:09 +0300

    Добавил метод первого шага для test_math_counting_objects_lesson_take_kitten_to_bus_stop

--- pages/math/counting_objects_lesson_take_kitten_to_bus_stop_page.py
+++ pages/math/counting_objects_lesson_take_kitten_to_bus_stop_page.py
@@ -6,4 +6,48 @@ from pages.base_page import BasePage
 
 
 class CountingObjectsLessonTakeKittenToBusStopPage(BasePage):
-    BUTTON_START = (By.XPATH, '(//div[contains(@class, "styles__StyledImage-sc-1ch9bzc-0 insPLH")])[2]')
+    INPUT = (By.XPATH, '//div[contains(@class, "focused")]')
+    BUTTON_KEY_0 = (By.XPATH, '//div[@data-value="0"]')
+    BUTTON_KEY_1 = (By.XPATH, '//div[@data-value="1"]')
+    BUTTON_KEY_2 = (By.XPATH, '//div[@data-value="2"]')
+    BUTTON_KEY_3 = (By.XPATH, '//div[@data-value="3"]')
+    BUTTON_KEY_4 = (By.XPATH, '//div[@data-value="4"]')
+    BUTTON_KEY_5 = (By.XPATH, '//div[@data-value="5"]')
+    BUTTON_KEY_6 = (By.XPATH, '//div[@data-value="6"]')
+    BUTTON_KEY_7 = (By.XPATH, '//div[@data-value="7"]')
+    BUTTON_KEY_8 = (By.XPATH, '//div[@data-value="8"]')
+    BUTTON_KEY_9 = (By.XPATH, '//div[@data-value="9"]')
+
+    PROGRESS_BAR_BUTTON_1_5_4 = (By.XPATH, '//div[@width="140.14705882352942"]')
+
+    def click_on_button_key_1_fill_input_1(self):
+        self.element_is_visible(self.INPUT)
+        self.element_is_visible(self.BUTTON_KEY_1).click()
+
+    def click_on_button_key_2_fill_input_2(self):
+        self.element_is_visible(self.INPUT)
+        self.element_is_visible(self.BUTTON_KEY_2).click()
+
+    def click_on_button_key_3_fill_input_3(self):
+        self.element_is_visible(self.INPUT)
+        self.element_is_visible(self.BUTTON_KEY_3).click()
+
+    def click_on_button_key_4_fill_input_4(self):
+        self.element_is_visible(self.INPUT)
+        self.element_is_visible(self.BUTTON_KEY_4).click()
+
+    def click_on_button_key_5_fill_input_5(self):
+        self.element_is_visible(self.INPUT)
+        self.element_is_visible(self.BUTTON_KEY_5).click()
+
+    def click_on_button_key_6_fill_input_6(self):
+        self.element_is_visible(self.INPUT)
+        self.element_is_visible(self.BUTTON_KEY_6).click()
+
+    def click_on_button_key_7_fill_input_7(self):
+        self.element_is_visible(self.INPUT)
+        self.element_is_visible(self.BUTTON_KEY_7).click()
+
+    def click_on_button_key_8_fill_input_8(self):
+        self.element_is_visible(self.INPUT)
+        self.element_is_visible(self.BUTTON_KEY_8).click()
--- tests/test_math_counting_objects.py
+++ tests/test_math_counting_objects.py
@@ -1,3 +1,5 @@
+import time
+
 from pages.math.counting_objects_lesson_take_kitten_to_bus_stop_page import CountingObjectsLessonTakeKittenToBusStopPage
 from data import UrlsCountingObjects
 
@@ -7,3 +9,13 @@ class TestMathCountingObjects:
     def test_math_counting_objects_lesson_take_kitten_to_bus_stop(self, driver, login):
         lesson_take_kitten_to_bus_stop = CountingObjectsLessonTakeKittenToBusStopPage(driver)
         lesson_take_kitten_to_bus_stop.open(UrlsCountingObjects.TAKE_KITTEN_TO_BUS_STOP)
+        lesson_take_kitten_to_bus_stop.click_on_button_key_1_fill_input_1()
+        lesson_take_kitten_to_bus_stop.click_on_button_key_2_fill_input_2()
+        lesson_take_kitten_to_bus_stop.click_on_button_key_3_fill_input_3()
+        lesson_take_kitten_to_bus_stop.click_on_button_key_4_fill_input_4()
+        lesson_take_kitten_to_bus_stop.click_on_button_key_5_fill_input_5()
+        lesson_take_kitten_to_bus_stop.click_on_button_key_6_fill_input_6()
+        lesson_take_kitten_to_bus_stop.click_on_button_key_7_fill_input_7()
+        lesson_take_kitten_to_bus_stop.click_on_button_key_8_fill_input_8()
+
+

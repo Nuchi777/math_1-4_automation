f4a79bb747f6c172bc72eba8b0c11ae52914c480
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2024-01-13 17:09:46 +0300

    Исправил test_math_first_second_third_lesson_who_in_which_carriage

--- pages/math/first_second_third_lesson_who_in_which_carriage_page.py
+++ pages/math/first_second_third_lesson_who_in_which_carriage_page.py
@@ -9,11 +9,11 @@ class FirstSecondThirdLessonWhoInWhichCarriagePage(BasePage):
     FIRST_CARRIAGE_CLICKABLE = (By.XPATH, '//div[@data-component-name="picBlockItem_1"]')
     FOURTH_CARRIAGE_CLICKABLE = (By.XPATH, '//div[@data-component-name="picBlockItem_4"]')
     ANIMALS = (By.XPATH, '//div[contains(@class, "DraggableItems")]')
+    ANIMAL_ACTIVE = (By.XPATH, '//div[contains(@class, "undefined active")]')
     FIRST_CARRIAGE_DROPPABLE = (By.XPATH, '(//div[contains(@class, "DropContainer")])[1]')
     SECOND_CARRIAGE_DROPPABLE = (By.XPATH, '(//div[contains(@class, "DropContainer")])[2]')
     THIRD_CARRIAGE_DROPPABLE = (By.XPATH, '(//div[contains(@class, "DropContainer")])[3]')
     FOURTH_CARRIAGE_DROPPABLE = (By.XPATH, '(//div[contains(@class, "DropContainer")])[4]')
-    CONGRAT_STATE_DEFAULT = (By.XPATH, '//div[contains(@class, "BoxesContainer")]//div[contains(@class, "state_default")]')
 
 
     def click_on_first_carriage(self):
@@ -22,26 +22,23 @@ class FirstSecondThirdLessonWhoInWhichCarriagePage(BasePage):
 
     def click_on_fourth_carriage(self):
         self.element_is_visible(self.FOURTH_CARRIAGE_CLICKABLE).click()
+        self.element_is_invisible(self.FOURTH_CARRIAGE_CLICKABLE)
 
     def drag_animal_in_third_carriage(self):
+        self.element_is_visible(self.ANIMAL_ACTIVE)
         self.drag_and_drop_on_to_element_new(self.ANIMALS, self.THIRD_CARRIAGE_DROPPABLE)
-        time.sleep(1)
-        self.element_is_presence(self.CONGRAT_STATE_DEFAULT)
 
     def drag_animal_in_first_carriage(self):
+        self.element_is_visible(self.ANIMAL_ACTIVE)
         self.drag_and_drop_on_to_element_new(self.ANIMALS, self.FIRST_CARRIAGE_DROPPABLE)
-        time.sleep(1)
-        self.element_is_presence(self.CONGRAT_STATE_DEFAULT)
 
     def drag_animal_in_second_carriage(self):
+        self.element_is_visible(self.ANIMAL_ACTIVE)
         self.drag_and_drop_on_to_element_new(self.ANIMALS, self.SECOND_CARRIAGE_DROPPABLE)
-        time.sleep(1)
-        self.element_is_presence(self.CONGRAT_STATE_DEFAULT)
 
     def drag_animal_in_fourth_carriage(self):
+        self.element_is_visible(self.ANIMAL_ACTIVE)
         self.drag_and_drop_on_to_element_new(self.ANIMALS, self.FOURTH_CARRIAGE_DROPPABLE)
-        time.sleep(1)
-        self.element_is_presence(self.CONGRAT_STATE_DEFAULT)
 
     def check_headbar_logo_is_displayed(self):
         assert self.element_is_visible(MainPageLocators.HEADBAR_LOGO).is_displayed()

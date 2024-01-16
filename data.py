420c5c3b4cd05b5e5d01956c8c4a239782a76daa
Evgeny Koloskov <evgeny.koloskov@mail.ru>
2024-01-16 19:55:59 +0300

    fix ursl

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 class Urls:
-    URL_MAIN_PAGE = 'https://uchi.ru/'
+    URL_MAIN_PAGE = 'https://uchi.ru'
 
 
 class UserData:
@@ -8,34 +8,34 @@ class UserData:
 
 
 class UrlsCosmicAdventure:
-    START = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F125281&card_id=436246&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F436246%3Fsubject_id%3D1&content_card_id=17518&options=progress_panel%2Canimated&subject_id=1'
-    EXPLORE_PLANET = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F125281&card_id=436247&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F436247%3Fsubject_id%3D1&content_card_id=17519&options=progress_panel%2Canimated&subject_id=1'
-    FIX_ROCKET = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F125281&card_id=436248&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F436248%3Fsubject_id%3D1&content_card_id=17520&options=progress_panel%2Canimated&subject_id=1'
-    SAVE_KNYSH = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F125281&card_id=436249&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fdiplom%2F436249%3Fsubject_id%3D1%26topic_id%3D13%26grade%3D1%26&content_card_id=17521&options=progress_panel%2Canimated&subject_id=1'
+    START = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F125281&card_id=436246&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F436246%3Fsubject_id%3D1&content_card_id=17518&options=progress_panel%2Canimated&subject_id=1'
+    EXPLORE_PLANET = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F125281&card_id=436247&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F436247%3Fsubject_id%3D1&content_card_id=17519&options=progress_panel%2Canimated&subject_id=1'
+    FIX_ROCKET = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F125281&card_id=436248&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F436248%3Fsubject_id%3D1&content_card_id=17520&options=progress_panel%2Canimated&subject_id=1'
+    SAVE_KNYSH = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F125281&card_id=436249&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fdiplom%2F436249%3Fsubject_id%3D1%26topic_id%3D13%26grade%3D1%26&content_card_id=17521&options=progress_panel%2Canimated&subject_id=1'
 
 
 class UrlsCountingObjects:
-    TAKE_KITTEN_TO_BUS_STOP = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F118&card_id=420&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F420%3Fsubject_id%3D1&content_card_id=420&options=progress_panel%2Canimated&subject_id=1'
-    WHICH_BUS_STOP_KITTEN = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F118&card_id=421&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F421%3Fsubject_id%3D1&content_card_id=421&options=progress_panel%2Canimated&subject_id=1'
+    TAKE_KITTEN_TO_BUS_STOP = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F118&card_id=420&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F420%3Fsubject_id%3D1&content_card_id=420&options=progress_panel%2Canimated&subject_id=1'
+    WHICH_BUS_STOP_KITTEN = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F118&card_id=421&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F421%3Fsubject_id%3D1&content_card_id=421&options=progress_panel%2Canimated&subject_id=1'
 
 
 class UrlsFirstSecondThird:
-    HOW_MANY_ATHLETES = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F18175&card_id=135056&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F135056%3Fsubject_id%3D1&content_card_id=8493&options=progress_panel%2Canimated&subject_id=1'
-    PORYADKOVYE_CHISLITELNYE = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F18175&card_id=135057&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F135057%3Fsubject_id%3D1&content_card_id=8494&options=progress_panel%2Canimated&subject_id=1'
-    WHO_IN_WHICH_CARRIAGE = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F18175&card_id=135058&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F135058%3Fsubject_id%3D1&content_card_id=10322&options=progress_panel%2Canimated&subject_id=1'
-    CHOOSE_ANSWER = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F18175&card_id=60903&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fdiplom%2F60903%3Fsubject_id%3D1%26topic_id%3D13%26grade%3D1%26&content_card_id=8495&options=progress_panel%2Canimated&subject_id=1'
+    HOW_MANY_ATHLETES = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F18175&card_id=135056&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F135056%3Fsubject_id%3D1&content_card_id=8493&options=progress_panel%2Canimated&subject_id=1'
+    PORYADKOVYE_CHISLITELNYE = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F18175&card_id=135057&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F135057%3Fsubject_id%3D1&content_card_id=8494&options=progress_panel%2Canimated&subject_id=1'
+    WHO_IN_WHICH_CARRIAGE = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F18175&card_id=135058&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F135058%3Fsubject_id%3D1&content_card_id=10322&options=progress_panel%2Canimated&subject_id=1'
+    CHOOSE_ANSWER = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F18175&card_id=60903&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fdiplom%2F60903%3Fsubject_id%3D1%26topic_id%3D13%26grade%3D1%26&content_card_id=8495&options=progress_panel%2Canimated&subject_id=1'
 
 
 class UrlsNumberLineAndNumberAxis:
-    LEARNING_NUMBER_ORDER = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F120&card_id=414&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F414%3Fsubject_id%3D1&content_card_id=414&options=progress_panel%2Canimated&subject_id=1'
-    ACQUAINTED_WITH_NUMBER_AXIS = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F120&card_id=442&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F442%3Fsubject_id%3D1&content_card_id=442&options=progress_panel%2Canimated&subject_id=1'
-    WHAT_POINT_GRASSHOPPER = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F120&card_id=377&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F377%3Fsubject_id%3D1&content_card_id=377&options=progress_panel%2Canimated&subject_id=1'
-    NUMBERS_IN_REVERSE_ORDER = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F120&card_id=443&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fdiplom%2F443%3Fsubject_id%3D1%26topic_id%3D13%26grade%3D1%26&content_card_id=443&options=progress_panel%2Canimated&subject_id=1'
+    LEARNING_NUMBER_ORDER = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F120&card_id=414&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F414%3Fsubject_id%3D1&content_card_id=414&options=progress_panel%2Canimated&subject_id=1'
+    ACQUAINTED_WITH_NUMBER_AXIS = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F120&card_id=442&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F442%3Fsubject_id%3D1&content_card_id=442&options=progress_panel%2Canimated&subject_id=1'
+    WHAT_POINT_GRASSHOPPER = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F120&card_id=377&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F377%3Fsubject_id%3D1&content_card_id=377&options=progress_panel%2Canimated&subject_id=1'
+    NUMBERS_IN_REVERSE_ORDER = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F1%2Flesson%2F120&card_id=443&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fdiplom%2F443%3Fsubject_id%3D1%26topic_id%3D13%26grade%3D1%26&content_card_id=443&options=progress_panel%2Canimated&subject_id=1'
 
 
 class UrlsSpeechAndLanguage:
-    WHAT_IS_SPEECH = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F2%2Flesson%2F3203&card_id=13478&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F13478%3Fsubject_id%3D2&content_card_id=3963&options=progress_panel%2Canimated&subject_id=2'
-    WHY_IS_SPEECH_NEEDED = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F2%2Flesson%2F3203&card_id=13479&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F13479%3Fsubject_id%3D2&content_card_id=3964&options=progress_panel%2Canimated&subject_id=2'
-    WHAT_SHOULD_SPEECH_BE_LIKE = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F2%2Flesson%2F3203&card_id=13480&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F13480%3Fsubject_id%3D2&content_card_id=3965&options=progress_panel%2Canimated&subject_id=2'
-    NATIVE_LANGUAGE = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F2%2Flesson%2F3203&card_id=13481&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F13481%3Fsubject_id%3D2&content_card_id=3966&options=progress_panel%2Canimated&subject_id=2'
-    LEARNING_VOCABULARY_WORDS = 'https://uchi.ru/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F2%2Flesson%2F3203&card_id=13482&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fdiplom%2F13482%3Fsubject_id%3D2%26topic_id%3D159%26grade%3D1%26&content_card_id=3967&options=progress_panel%2Canimated&subject_id=2'
+    WHAT_IS_SPEECH = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F2%2Flesson%2F3203&card_id=13478&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F13478%3Fsubject_id%3D2&content_card_id=3963&options=progress_panel%2Canimated&subject_id=2'
+    WHY_IS_SPEECH_NEEDED = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F2%2Flesson%2F3203&card_id=13479&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F13479%3Fsubject_id%3D2&content_card_id=3964&options=progress_panel%2Canimated&subject_id=2'
+    WHAT_SHOULD_SPEECH_BE_LIKE = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F2%2Flesson%2F3203&card_id=13480&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F13480%3Fsubject_id%3D2&content_card_id=3965&options=progress_panel%2Canimated&subject_id=2'
+    NATIVE_LANGUAGE = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F2%2Flesson%2F3203&card_id=13481&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fcard%2F13481%3Fsubject_id%3D2&content_card_id=3966&options=progress_panel%2Canimated&subject_id=2'
+    LEARNING_VOCABULARY_WORDS = f'{Urls.URL_MAIN_PAGE}''/exercise/player?back_path=%2Fprofile%2Fstudents%2Fstudy%2F2%2Flesson%2F3203&card_id=13482&congrat_path=%2Fprofile%2Fstudents%2Fcongrats%2Fdiplom%2F13482%3Fsubject_id%3D2%26topic_id%3D159%26grade%3D1%26&content_card_id=3967&options=progress_panel%2Canimated&subject_id=2'
